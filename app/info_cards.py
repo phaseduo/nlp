629e3a7098bae0f9049f6a4e2e9e1b681f2a433b
Alastair Paragas <alastairparagas@gmail.com>
2017-02-19 02:20:57 -0500

    NER enriched

--- app/info_cards.py
+++ app/info_cards.py
@@ -1,3 +1,69 @@
+from multiprocessing.pool import ThreadPool
+from summa import keywords
+import search_word
+import spacy
+import numpy
+
+
+nlp = spacy.load("en")
+
+
+def _keywords_(corpus):
+  """
+  Find important keywords - use TextRank algorithm to find
+  most important keywords, then a fuzzier/dumber match with
+  NER tagging
+  :param corpus:
+  :return:
+  """
+  keywords_list = keywords.keywords(corpus).split("\n")
+
+  def label_normalizer(spacy_label):
+    label = {
+      "PERSON": "Person",
+      "NORP": "Nationality/Religious/Political Groups",
+      "FACILITY": "Facility",
+      "ORG": "Organization",
+      "GPE": "Geographic Location",
+      "LOC": "Location",
+      "PRODUCT": "Thing",
+      "EVENT": "Event",
+      "WORK_OF_ART": "Art",
+      "LANGUAGE": "Language",
+    }
+    return label.get(spacy_label, spacy_label.title())
+  entity_tuples = [
+    (label_normalizer(entity.label_), entity.text)
+    for entity in nlp(corpus).ents
+  ]
+
+  if len(keywords_list) >= 2:
+    '''
+    If keyword in keywords_list fuzzy match with a keyword in NER
+    generated list, do a transform to obtain that NER's provided
+    entity category
+    '''
+    def transformer(keyword):
+      for (entity_category, entity_text) in entity_tuples:
+        if keyword.lower() in entity_text.lower():
+          return entity_category, keyword
+        if entity_text.lower() in keyword.lower():
+          return entity_category, keyword
+
+      return "", keyword
+    return map(transformer, keywords_list)
+
+  return entity_tuples
+
+
+def _map_info_thread_((label, word)):
+  results_list = search_word.search_word(word)
+
+  def transformer(result):
+    result["entity"] = label
+    return result
+  return map(transformer, results_list)
+
 
 def sentence(sentence):
   """
@@ -5,7 +71,17 @@ def sentence(sentence):
   :param sentence:
   :return: [{topic, shortDescription, description, entity, imgUrl}]
   """
-  pass
+  thread_pool = ThreadPool(3)
+  keywords_list = _keywords_(corpus)
+
+  cards = thread_pool.map(
+    _map_info_thread_,
+    keywords_list
+  )
+  thread_pool.close()
+  thread_pool.join()
+
+  return [item for sublist in cards for item in sublist]
 
 
 def corpus(corpus):
@@ -14,4 +90,14 @@ def corpus(corpus):
   :param corpus:
   :return: [{topic, shortDescription, description, entity, imgUrl}]
   """
-  pass
+  thread_pool = ThreadPool(20)
+  keywords_list = _keywords_(corpus)
+
+  cards = thread_pool.map(
+    _map_info_thread_,
+    keywords_list
+  )
+  thread_pool.close()
+  thread_pool.join()
+
+  return [item for sublist in cards for item in sublist]
